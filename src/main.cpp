24c94d9fcbd526ec06d726549a99d6534de97b74
Mostafa Elshafie <elshafie1912@gmail.com>
2021-09-17 21:52:27 +0200

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@ SoftwareSerial myserial (2,3) ;
   int IN2 =  7;
   int IN1 = 6;
 
+  int JX = A5;
+  int JY = A4;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -18,17 +21,31 @@ void setup() {
   pinMode(IN2 , OUTPUT);
   pinMode(ENB , OUTPUT);
   pinMode(ENA , OUTPUT);
+
+  // Joystick 
+  pinMode(JX , INPUT);
+  pinMode(JY , INPUT);
+  
   myserial.begin(9600); 
   Serial.begin (9600) ;  
  
 }
+
 void moveForward(int A,int  B){
   digitalWrite(IN1 , 0);
   digitalWrite(IN2 , 1);
-  analogWrite(ENB, A);
+  // analogWrite(ENB, A);
   digitalWrite(IN3 , 1);
   digitalWrite(IN4 , 0);
-  analogWrite(ENA, B);
+  // analogWrite(ENA, B);
+}
+void moveBackward(int A,int  B){
+  digitalWrite(IN1 , 1);
+  digitalWrite(IN2 , 0);
+  // analogWrite(ENB, A);
+  digitalWrite(IN3 , 0);
+  digitalWrite(IN4 , 1);
+  // analogWrite(ENA, B);
 }
  void turnRightHard(){
   moveForward(70, 50);  }
@@ -45,25 +62,96 @@ void stop(){
 void loop() {
   // put your main code here, to run repeatedly:
   
-
-  if(myserial.available() )
-  {
-    String data = (String) myserial.readString();
-     Serial.print( data ); 
-    if(data == "trh"){
-      turnRightHard();
-      Serial.println("turning");
-  }
-    else if (data = "stop")
-    {
-      stop();
-    }
+  Serial.println("JX=");
+  Serial.println(analogRead(JX));
     
+  Serial.println("JY=" );
+  Serial.println(analogRead(JY));
+  int motorSpeedA;
+  int motorSpeedB;
+  int vJX = analogRead(JX);
+  int vJY = analogRead(JY);
+  
+  if(vJY < 470){
+      moveForward(1,1);
+      motorSpeedA = map(vJY, 470, 0, 0, 127);
+      motorSpeedB = map(vJY, 470, 0, 0, 127);  
+      // moveForward(motorSpeedA, motorSpeedB);
+  } else if (vJY > 550) 
+  {
+      moveBackward(1,1);
+    motorSpeedA = map(vJY, 550, 1023, 0, 127);
+    motorSpeedB = map(vJY, 550, 1023, 0, 127);
+    // moveBackward(motorSpeedA, motorSpeedB);
   }
-    if (Serial.available () )
+  else 
+  {
+    motorSpeedA = 0;
+    motorSpeedB = 0;
+    // moveForward(motorSpeedA, motorSpeedB);
+  }
+
+  if (vJX < 470) {
+   
+    int xMapped = map(vJX, 470, 0, 0, 127);
+    motorSpeedA = motorSpeedA - xMapped*2;
+    motorSpeedB = motorSpeedB + xMapped*2;
+    
+    if (motorSpeedA < 0) 
     {
-      myserial.write(Serial.read() );
-      }
+      motorSpeedA = 0;
+    }
+    if (motorSpeedB > 127) 
+    {
+      motorSpeedB = 127;
+    }
+ }
+     if (vJX > 550) 
+    {
+    int xMapped = map(vJX, 550, 1023, 0, 127);
+    motorSpeedA = motorSpeedA + xMapped*2;
+    motorSpeedB = motorSpeedB - xMapped*2;
+    if (motorSpeedA > 127) 
+    {
+      motorSpeedA = 127;
+    }
+    if (motorSpeedB < 0) 
+    {
+      motorSpeedB = 0;
+    }
+  }
+ 
+  if (motorSpeedA < 70) 
+  {
+    motorSpeedA = 0;
+  }
+  if (motorSpeedB < 70) 
+  {
+    motorSpeedB = 0;
+  }
+  analogWrite(ENA, motorSpeedA);
+  analogWrite(ENB, motorSpeedB);
+  // if(myserial.available() )
+  // {
+  //   String data = (String) myserial.readString();
+  //    Serial.print( data ); 
+  //   if(data == "trh"){
+  //     turnRightHard();
+  //     Serial.println("turning");
+  // }
+  //   else if (data = "stop")
+  //   {
+  //     stop();
+  //   }
+    
+  // }
+  //   if (Serial.available () )
+  //   {
+  //     myserial.write(Serial.read() );
+  //     }
+
+
+     
 
 
 
